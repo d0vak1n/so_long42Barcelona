ea852c0456a238fcdd5644aed725e2387680d518
d0vak1n <rulanxa@gmail.com>
2024-06-26 13:11:34 +0200

    added minilibX

--- Makefile
+++ Makefile
@@ -9,5 +9,38 @@
 #    Updated: 2024/05/06 12:31:10 by ramoreno         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
-
+## Name of the program
 NAME=so_long
+
+## Compile flags
+CC=gcc
+CFLAGS=-Wall -Wextra -Werror
+XFLAGS=-lmlx -lXext -lX11
+
+## Source files
+FILES=so_long.c
+
+OBJS=$(patsubst %.c, %.o, $(FILES))
+
+$(NAME): $(OBJS) Makefile lib_so_long.h
+	@echo "\n📝 Compiling program $(NAME) ... 📝\n"
+	$(CC) $(CFLAGS) -o $(NAME) $(OBJS) $(XFLAGS)
+	@echo "\n📖 $(NAME) created succesfully 📖\n"
+
+all: $(NAME)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $^
+
+.PHONY: all clean fclean re
+
+clean:
+	@rm -f $(OBJS)
+	@echo "\n 🗑 All .o deleted 🗑\n"
+
+fclean: clean
+	@rm -rf $(NAME)
+	@echo "\n 🗑 All .a deleted 🗑\n"
+
+re: fclean all
+	@echo "\n🔁 Relinked 🔁\n"
--- so_long.c
+++ so_long.c
@@ -9,5 +9,31 @@
 /*   Updated: 2024/05/06 12:45:32 by ramoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <mlx.h>
 
+typedef struct s_data
+{
+    void *img;
+    char *addr;
+    int bits_per_pixel;
+    int line_length;
+    int endian;
+} t_data;
 
+int main(void)
+{
+    void *mlx;
+    t_data img;
+
+    mlx = mlx_init();
+    img.img = mlx_new_image(mlx, 1920, 1080);
+
+    /*
+    ** After creating an image, we can call `mlx_get_data_addr`, we pass
+    ** `bits_per_pixel`, `line_length`, and `endian` by reference. These will
+    ** then be set accordingly for the *current* data address.
+    */
+    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+                                 &img.endian);
+    mlx_loop(mlx);
+}
