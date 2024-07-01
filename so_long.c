0276be98fb071bf59d46812dd2245693979d2a93
d0vak1n <rulanxa@gmail.com>
2024-07-01 16:00:23 +0200

    testing main

--- so_long.c
+++ so_long.c
@@ -3,37 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ramoreno <ramoreno@student.42barcel>       +#+  +:+       +#+        */
+/*   By: ramoreno <ramoreno@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/06 12:45:30 by ramoreno          #+#    #+#             */
-/*   Updated: 2024/05/06 12:45:32 by ramoreno         ###   ########.fr       */
+/*   Created: 2024/07/01 13:03:50 by ramoreno          #+#    #+#             */
+/*   Updated: 2024/07/01 13:03:50 by ramoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <mlx.h>
+#include "./minilibx_linux/mlx.h"
 
-typedef struct s_data
+int	main(void)
 {
-    void *img;
-    char *addr;
-    int bits_per_pixel;
-    int line_length;
-    int endian;
-} t_data;
+	void	*mlx;
+	void	*mlx_win;
 
-int main(void)
-{
-    void *mlx;
-    t_data img;
-
-    mlx = mlx_init();
-    img.img = mlx_new_image(mlx, 1920, 1080);
-
-    /*
-    ** After creating an image, we can call `mlx_get_data_addr`, we pass
-    ** `bits_per_pixel`, `line_length`, and `endian` by reference. These will
-    ** then be set accordingly for the *current* data address.
-    */
-    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-                                 &img.endian);
-    mlx_loop(mlx);
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
 }
