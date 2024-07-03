eeb96388f020ff2c6b788a4afcd220e9cf9d6856
d0vak1n <rulanxa@gmail.com>
2024-07-03 17:57:40 +0200

    defined size of every sprite

--- src/so_long.c
+++ src/so_long.c
@@ -9,6 +9,15 @@
 /*   Updated: 2024/07/01 13:03:50 by ramoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+Dimensiones elegidas para el juego, 1920x1080
+1920/16 = 120
+1080/9 = 120
+
+Cada sprite sera de 120x120
+
+*/
 #include "../minilibx_linux/mlx.h"
 #include "lib_so_long.h"
 #include <stdio.h>
@@ -17,7 +26,7 @@ void my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char *dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 256));
 	*(unsigned int *)dst = color;
 }
 
@@ -26,13 +35,13 @@ int main(void)
 	void *mlx;
 	void *img;
 	void *mlx_win;
-	char *relative_path = "./img/xpm/coin.xpm";
+	char *relative_path = "./img/xpm/cofre.xpm";
 	int img_width;
 	int img_height;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 720, 420, "Hello world!");
-	img = mlx_new_image(mlx, 720, 420);
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img = mlx_new_image(mlx, 1920, 1080);
 	img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
 	if (!img)
 	{
