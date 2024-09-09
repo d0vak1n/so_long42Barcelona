93fb5d6f72624385e42d7d6fda7571009cc94fac
Raul <rulanxa@gmail.com>
2024-09-09 19:26:56 +0200

    refact name and handling no found file

--- src/ft_checkmap.c
+++ src/ft_checkmap.c
@@ -10,7 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int checkmap(int f)
-{
+#include "lib_so_long.h"
+#include <fcntl.h>
 
+int ft_checkmap(char* f)
+{
+	int fd = open(f, O_RDONLY);
+	if (fd == -1)
+	{
+		ft_write("Error\nFile not found\n");
+		return (1);
+	}
 }
