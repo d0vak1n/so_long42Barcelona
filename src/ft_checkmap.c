cf07b480da9bcde577634a6adaa0e31cad5076c4
d0vak1n <rulanxa@gmail.com>
2024-10-02 19:35:51 +0200

    checkwalls started

--- src/ft_checkmap.c
+++ src/ft_checkmap.c
@@ -6,7 +6,7 @@
 /*   By: ramoreno <ramoreno@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 13:12:21 by ramoreno          #+#    #+#             */
-/*   Updated: 2024/10/02 18:27:37 by ramoreno         ###   ########.fr       */
+/*   Updated: 2024/10/02 19:34:38 by ramoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 #include "../get_next_line/get_next_line.h"
 #include <stdio.h>
 
-void	_map_error(char *str)
+static void	_map_error(char *str)
 {
 	ft_write(str);
 	exit(1);
 }
 
-int	_check_size(int fd)
+static int	_check_size(int fd)
 {
 	char	*line;
 	int		prev_x;
@@ -50,6 +50,37 @@ int	_check_size(int fd)
 	return (1);
 }
 
+static int	_check_walls(int fd)
+{
+	char	*line;
+	char	*prev_line;
+	int		i;
+
+	i = 0;
+	line = get_next_line(fd);
+	if (!line)
+		_map_error("Error\nEmpty file\n");
+	while (line[++i]) // Priemra linea
+	{
+		if (line[i] != '1')
+			_map_error("Error\nMap is not surrounded by walls1");
+	}
+	while (line) //Segunda linea
+	{
+		prev_line = line;
+		if (line[0] != '1' || line[ft_strlen(line) - 1] != '1')
+			ft_write("toma toma2\n");
+		line = get_next_line(fd);
+	}
+	i = -1;
+	while (prev_line[++i]) // ultima linea
+	{
+		if (line[i] != '1')
+			_map_error("Error\nMap is not surrounded by walls3");
+	}
+	return (0);
+}
+
 int	ft_checkmap(char *f)
 {
 	int	fd;
@@ -59,8 +90,9 @@ int	ft_checkmap(char *f)
 		_map_error("Error\nFile not found\n");
 	if (_check_size(fd) == 0)
 	{
-		ft_write("Map is rectangular\n");
-		return (0);
+		fd = open(f, O_RDONLY);
+		if (_check_walls(fd) == 0)
+			_map_error("Map is surrounded by walls\n");
 	}
 	else
 		_map_error("Error\nMap is not rectangular\n");
